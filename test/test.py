ab9afddcd7fc84e1948ab9517ece68d7b7dee7ec
Kasper Laursen <kasper.laursen@gmail.com>
2016-07-17 21:06:40 +0200

    test.py: TODO: Wait for all processes to finish, then verirfy that there is a new pdf for all scenarios, write nice output and proper newline.

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ biblio = {'bibtex', 'biblatex'}
 texengines = {
     'latex-ps'  :'-ps -pdfps',
     'latex-dvi' :'-dvi -pdfdvi',
-    'pdflatex'  :'-pdf',
+    'latex-pdf' :'-pdf',
     'xelatex'   :'-xelatex', 
     'lualatex'  :'-lualatex',
 }
@@ -15,3 +15,5 @@ for tex, arg in texengines.iteritems():
     for b in biblio:
         execute = 'latexmk %s -jobname=test_%s_%s  -f -gg example_%s.tex' % (arg, tex, b, b)
         subprocess.Popen(execute, shell=True)
+
+#TODO: Wait for all processes to finish, then verirfy that there is a new pdf for all scenarios, write nice output and proper newline.
